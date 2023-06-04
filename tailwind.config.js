ad0d918c7e1d1610f71190d016d248e2a1818ac2
Michael Nau <michaelwnau@gmail.com>
2023-06-04 17:27:32 -0500

    rebase nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 // tailwind.config.js
 
 module.exports = {
-    - content: [],
-    + content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    };
+    purge: {
+      content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+    },
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
